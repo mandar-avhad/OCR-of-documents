30b679d509db961bca7e96437aec368a6fe50300
Mandar <61730638+mandar-avhad@users.noreply.github.com>
2021-02-25 00:10:57 +0530

    Update Object_detection_image.py

--- Object_detection_image.py
+++ Object_detection_image.py
@@ -1,20 +1,3 @@
-######## Image Object Detection Using Tensorflow-trained Classifier #########
-#
-# Author: Evan Juras
-# Date: 1/15/18
-# Description: 
-# This program uses a TensorFlow-trained neural network to perform object detection.
-# It loads the classifier and uses it to perform object detection on an image.
-# It draws boxes, scores, and labels around the objects of interest in the image.
-
-## Some of the code is copied from Google's example at
-## https://github.com/tensorflow/models/blob/master/research/object_detection/object_detection_tutorial.ipynb
-
-## and some is copied from Dat Tran's example at
-## https://github.com/datitran/object_detector_app/blob/master/object_detection_app.py
-
-## but I changed it to make it more understandable to me.
-
 # Import packages
 import os
 import cv2
@@ -22,7 +5,6 @@ import numpy as np
 import tensorflow.compat.v1 as tf
 import sys
 
-# This is needed since the notebook is stored in the object_detection folder.
 sys.path.append("..")
 
 # Import utilites
@@ -51,9 +33,6 @@ NUM_CLASSES = 4
 
 # Load the label map.
 # Label maps map indices to category names, so that when our convolution
-# network predicts `5`, we know that this corresponds to `king`.
-# Here we use internal utility functions, but anything that returns a
-# dictionary mapping integers to appropriate string labels would be fine
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
@@ -69,17 +48,14 @@ with detection_graph.as_default():
 
     sess = tf.Session(graph=detection_graph)
 
-# Define input and output tensors (i.e. data) for the object detection classifier
+# Define input and output tensors for the object detection classifier
 
 # Input tensor is the image
 image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
 
 # Output tensors are the detection boxes, scores, and classes
-# Each box represents a part of the image where a particular object was detected
 detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
 
-# Each score represents level of confidence for each of the objects.
-# The score is shown on the result image, together with the class label.
 detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
 detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
 
@@ -87,19 +63,16 @@ detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
 num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 
 # Load image using OpenCV and
-# expand image dimensions to have shape: [1, None, None, 3]
-# i.e. a single-column array, where each item in the column has the pixel RGB value
-##image = cv2.imread(PATH_TO_IMAGE)
 image = cv2.imread(IMAGE_NAME)
 image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 image_expanded = np.expand_dims(image_rgb, axis=0)
 
-# Perform the actual detection by running the model with the image as input
+
 (boxes, scores, classes, num) = sess.run(
     [detection_boxes, detection_scores, detection_classes, num_detections],
     feed_dict={image_tensor: image_expanded})
 
-# Draw the results of the detection (aka 'visulaize the results')
+# Draw the results of the detection
 
 vis_util.visualize_boxes_and_labels_on_image_array(
     image,
@@ -111,13 +84,10 @@ vis_util.visualize_boxes_and_labels_on_image_array(
     line_thickness=6,
     min_score_thresh=0.60)
 
-# All the results have been drawn on image. Now display the image.
+#display the image.
 #cv2.imshow('Object detector', image)
 cv2.imwrite('outputSS5.jpg',image)
-#cv2.imwrite(IMAGE_NAME+"-model.jpg",image)
 
-# Press any key to close the image
 cv2.waitKey(0)
 
-# Clean up
 cv2.destroyAllWindows()
